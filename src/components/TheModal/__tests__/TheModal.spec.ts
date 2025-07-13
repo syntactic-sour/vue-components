a9bcfb571b1e03080964838fdd56176ebb5261ce
syntactic-sour <196000088+syntactic-sour@users.noreply.github.com>
2025-07-13 20:59:11 +0400

    test: fix unit for Modal

--- src/components/TheModal/__tests__/TheModal.spec.ts
+++ src/components/TheModal/__tests__/TheModal.spec.ts
@@ -4,8 +4,10 @@ import { mount } from '@vue/test-utils'
 import TheModal from '../TheModal.vue'
 
 describe('TheModal', () => {
-  it('renders properly', () => {
+  it('renders properly', async () => {
     const wrapper = mount(TheModal, { props: { heading: 'Hello Vitest' } })
-    expect(wrapper.text()).toContain('Hello Vitest')
+    await wrapper.get('button').trigger('click')
+    const dialog = document.body.querySelector('dialog')
+    expect(dialog?.innerText).toContain('Hello Vitest')
   })
 })
--- vitest.config.ts
+++ vitest.config.ts
@@ -8,6 +8,12 @@ export default mergeConfig(
     test: {
       environment: 'jsdom',
       root: fileURLToPath(new URL('./', import.meta.url)),
+      browser: {
+        provider: 'playwright', // or 'webdriverio'
+        enabled: true,
+        // at least one instance is required
+        instances: [{ browser: 'chromium' }],
+      },
     },
   }),
 )
