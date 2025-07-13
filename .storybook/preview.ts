d0e296a4c614b7b23e29e86ebe811f607c20db9f
syntactic-sour <196000088+syntactic-sour@users.noreply.github.com>
2025-07-13 20:30:00 +0400

    feat: global styles for modal layout

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -18,6 +18,22 @@ const preview: Preview = {
       test: 'todo',
     },
   },
+
+  decorators: [
+    // 👇 Defining the decorator in the preview file applies it to all stories
+    (_, { parameters }) => {
+      // 👇 Make it configurable by reading from parameters
+      const { pageLayout } = parameters
+      switch (pageLayout) {
+        case 'page':
+          // Your page layout is probably a little more complex than this ;)
+          return { template: '<div id="app"><story/></div>' }
+        default:
+          // In the default case, don't apply a layout
+          return { template: '<story/>' }
+      }
+    },
+  ],
 }
 
 export default preview
